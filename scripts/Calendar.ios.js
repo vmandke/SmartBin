647fb2e0e3b3ae576ea8c69a250c5c463ab564a7
Vinaya Mandke <vinayamandke@gmail.com>
2015-10-18 19:34:47 +0530

    Dirty FIX for IOS Cal

--- scripts/Calendar.ios.js
+++ scripts/Calendar.ios.js
@@ -29,6 +29,7 @@ var Calendar = React.createClass({
 
     onDateSelect: function (date) {
         console.log(date);
+        this._setModalVisible(false)
     },
     handleClick: function() {
         //need to render Calendar
@@ -44,39 +45,30 @@ var Calendar = React.createClass({
 
         return (
            <View>
-            
-
-            <Modal
+           <Modal
               animated={true}
               transparent={false}
               visible={this.state.modalVisible}>
               <View style={[styles.container, modalBackgroundStyle]}>
                 <View style={[styles.innerContainer, innerContainerTransparentStyle]}>
                   <CalendarView
-                      scrollEnabled={true}              // False disables swiping. Default: True
-                      showControls={true}               // False hides prev/next buttons. Default: False
-                      titleFormat={'MMMM YYYY'}         // Format for displaying current month. Default: 'MMMM YYYY'
-                      dayHeadings={['S', 'M', 'T', 'W', 'T', 'F', 'S']}               // Default: ['S', 'M', 'T', 'W', 'T', 'F', 'S']
-                      prevButtonText={'Prev'}           // Text for previous button. Default: 'Prev'
-                      nextButtonText={'Next'}           // Text for next button. Default: 'Next
-                      onDateSelect={(date) => this.onDateSelect(date)} // Callback after date selection
-                      onTouchPrev={this.onTouchPrev}    // Callback for prev touch event
-                      onTouchNext={this.onTouchNext}    // Callback for next touch event
-                      onSwipePrev={this.onSwipePrev}    // Callback for back swipe event
-                      onSwipeNext={this.onSwipeNext}    // Callback for forward swipe event
-                      eventDates={['2015-07-01']}       // Optional array of moment() parseable dates that will show an event indicator
-                      startDate={'2015-08-01'}          // The first month that will display. Default: current month
-                      selectedDate={'2015-08-15'}       // Day to be selected
-                      // Customize any pre-defined styles
-                      customStyle={{day: {fontSize: 15, textAlign: 'center'}}}  />      
+                    ref="calendar"
+                    eventDates={[]}
+                    scrollEnabled={false}
+                    showControls={true}
+                    dayHeadings={['  Sun   ', '  Mon   ', '   Tue   ', '   Wed   ', '   Thu   ', '    Fri   ', '     Sat  ']}
+                    titleFormat={'MMMM YYYY'}
+                    prevButtonText={'Prev       '}
+                    nextButtonText={'       Next'}
+                    onDateSelect={(date) => this.onDateSelect(date)}
+                    onTouchPrev={() => console.log('Back TOUCH')}
+                    onTouchNext={() => console.log('Forward TOUCH')}
+                    onSwipePrev={() => console.log('Back SWIPE')}
+                    onSwipeNext={() => console.log('Forward SWIPE')}/>
                 </View>
               </View>
             </Modal>
-
-
-
-
-          <View style={styles.container}>
+            <View style={styles.container}>
               <TouchableOpacity onPress={this.handleClick}>
                 <Text style={styles.instructions}>
                   Click me
